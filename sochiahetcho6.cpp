0486eb5b186db7e59217db533d7dbd11940b302a
NCanhhieu <109597988+NCanhhieu@users.noreply.github.com>
2022-07-29 18:11:50 +0700

    update

--- sochiahetcho6.cpp
+++ sochiahetcho6.cpp
@@ -4,22 +4,15 @@ int main () {
 	printf(" nhap n :  ");
 	int n;
 	scanf("%d", &n);
-	int k = n;
-	if ( n < 6) {
-		k = 6;
-		printf(" so lon hon n be nhat chia het cho 2 va 3 la %d \n", k);
-	} else 
-	{
-		if ( n%6 == 0) {
-			k = n + 6;
-		printf(" so lon hon n be nhat chia het cho 2 va 3 la %d \n", k);
-		} else {
-			while ( k% 6 != 0 ) {
-				k++;
-			}
-		printf(" so lon hon n be nhat chia het cho 2 va 3 la %d \n", k);
-		}
-		
-	}
+	int k = n -1 ;
+	int min = 0;
+     while ( k > 0 ) {
+     	if ( k%2 == 0 && k%3==0 ) {
+     		min = k;
+     		break;
+		 }
+		 k--;
+	 }
+	 printf( " gia tri can tim %d ", min);
 	return 0;
 }
